9691c08b748b0d1283b61163105c608f0e4c60a7
AjayShukla007 <shuklaajay0838@gmail.com>
2023-12-06 16:31:51 +0530

    deploy

---
+++ src/Display.tsx
@@ -0,0 +1,35 @@
+import { DisplayState, formatTime } from "./helpers";
+
+interface DisplayProps {
+  displayState: DisplayState;
+  reset: () => void;
+  startStop: (displayState: DisplayState) => void;
+}
+
+const Display: React.FC<DisplayProps> = ({
+  displayState,
+  reset,
+  startStop,
+}) => {
+  return (
+    <div className="display">
+      <h4 id="timer-label">{displayState.timeType}</h4>
+      <span
+        id="time-left"
+        style={{ color: `${displayState.timerRunning ? "red" : "white"}` }}
+      >
+        {formatTime(displayState.time)}
+      </span>
+      <div>
+        <button id="start_stop" onClick={() => startStop(displayState)}>
+          {displayState.timerRunning ? "Pause" : "play"}
+        </button>
+        <button id="reset" onClick={reset}>
+          undo
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Display;
